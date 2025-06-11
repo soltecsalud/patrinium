5e71c6c0c2a468ed1985ec5a0528c32940cddd60
santiago erazo ramirez <serazo@soltecsalud.com>
2025-06-11 16:16:38 -0500

    vista con body GET de POST  SDER

--- views/router.php
+++ views/router.php
@@ -1,6 +1,6 @@
 <?php
 // Detectar método POST con JSON en el body
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $body = json_decode(file_get_contents('php://input'), true);
 
     if (!empty($body['route']) && $body['route'] === 'api-alertas') {
