dada515fc0a455562da67cca139690f44a2f8ebd
santiago erazo ramirez <serazo@soltecsalud.com>
2025-06-11 15:33:31 -0500

    modelo actualizado para alertas SDER

--- model/apiAlertasModel.php
+++ model/apiAlertasModel.php
@@ -6,7 +6,24 @@ class apiAlertasModel
     static public function mdlGetAlertas()
     {
         try {
-            $sql = "SELECT id_alerta, mensaje, tipo_alerta, fecha_creacion FROM alertas";
+            $sql = "SELECT 
+                    f.id as system_number, 
+                    f.datos->>'invoice_number' AS invoice_number,
+                    f.datos->>'logo' AS logo,
+                    SUM(
+                        (s.value->>'valor')::numeric * 
+                        (s.value->>'cantidad')::numeric
+                    ) AS total_calculado, created_at as fecha
+                    
+                    FROM 
+                    factura f,
+                    jsonb_each(f.datos->'servicios') AS s(key, value)
+                    where 
+                    f.estado = 2 -- porque estado 2 es no pagada
+                    GROUP BY 
+                    f.id, f.datos->>'invoice_number', f.datos->>'logo'
+                    order by 1 ASC;
+                        ";
             $consulta = Conexion::conectar()->prepare($sql);
             $consulta->execute();
 
